980a36fb8ad6c1ce256e92bcccbbcfedd59342d7
jeffk03 <97927684+jeffk03@users.noreply.github.com>
2024-05-24 11:25:54 -0300

    Create function: categorical_box

--- src/analysis/graph_generator.py
+++ src/analysis/graph_generator.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import seaborn as sns 
+import pandas as pd
 
 class VisualizationCreator:
 
@@ -8,3 +9,13 @@ class VisualizationCreator:
         The class has functions, which are responsible for creating graphs.
         '''
         self.df = df
+
+    def categorical_box(self,col):
+        cate = pd.DataFrame(self.df[col].value_counts())
+        x = cate.index
+        y = cate['count']
+        plt.bar(x, y)
+        plt.xlabel(col)
+        plt.ylabel('Count')
+        plt.title(f'Quantity for {col}')
+        plt.show()
