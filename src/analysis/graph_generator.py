987badb2822b0b1bb5a737e51d9998b21dbf25c4
jeffk03 <97927684+jeffk03@users.noreply.github.com>
2024-05-24 11:34:38 -0300

    Update function

--- src/analysis/graph_generator.py
+++ src/analysis/graph_generator.py
@@ -9,13 +9,18 @@ class VisualizationCreator:
         The class has functions, which are responsible for creating graphs.
         '''
         self.df = df
+    
+    def p(self):
+        print('teste')
 
     def categorical_box(self,col):
         cate = pd.DataFrame(self.df[col].value_counts())
         x = cate.index
-        y = cate['count']
+        y = cate.iloc[:,0]
         plt.bar(x, y)
-        plt.xlabel(col)
+        plt.xlabel(col,rotation=45, ha='right')
         plt.ylabel('Count')
         plt.title(f'Quantity for {col}')
+        for i, v in enumerate(y):  # Adiciona os valores dentro do gráfico
+            plt.text(i, v + 0.1, str(v), ha='center'
         plt.show()
