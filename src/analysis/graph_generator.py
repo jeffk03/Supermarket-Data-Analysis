28a6c93c9e562799f5889228b1bc21d5acbeb238
jeffk03 <97927684+jeffk03@users.noreply.github.com>
2024-05-28 10:17:56 -0300

    criado dist_plot

--- src/analysis/graph_generator.py
+++ src/analysis/graph_generator.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import seaborn as sns 
 import pandas as pd
+import numpy as np
+from scipy.stats import gaussian_kde
+
 
 class VisualizationCreator:
 
@@ -13,13 +16,51 @@ class VisualizationCreator:
     def p(self):
         print('teste')
 
-    def categorical_box(self,col):
+    def categorical_box(self,col,grid = True):
         cate = pd.DataFrame(self.df[col].value_counts())
         x = cate.index
         y = cate.iloc[:,0]
         plt.bar(x, y)
-        plt.xlabel(col,rotation=45, ha='right')
+        plt.xlabel(col)
         plt.ylabel('Count')
-        plt.grid(True)       
+        plt.grid(grid)       
         plt.title(f'Quantity for {col}') 
         plt.show()
+
+    def dist_plot(self,col,bins = 30):        
+      """
+      Gera um gráfico de distribuição para uma coluna específica de um DataFrame.
+      """
+      # Verifica se a coluna existe no DataFrame
+      if col not in self.df.columns:
+        raise ValueError(f"A coluna '{col}' não existe no DataFrame.")
+      
+      # Configura o estilo do seaborn
+      sns.set(style='whitegrid')
+      # Cria o gráfico de distribuição
+      plt.figure(figsize=(10, 6))
+      sns.histplot(self.df[col], kde=True, bins=bins, color='blue')
+
+      # Calcula a KDE
+      data = self.df[col].dropna()
+      kde = gaussian_kde(data)
+      x_range = np.linspace(data.min(), data.max(), 1000)
+      kde_values = kde(x_range)
+
+      # Plota a linha KDE
+      plt.plot(x_range, kde_values, color='red', lw=2, label='KDE')
+
+      # Título e rótulos
+      plt.title(f'Distribuição da coluna: {col}', fontsize=16)
+      plt.xlabel(col,fontsize =14)
+      plt.ylabel('Frequência',fontsize = 14)
+
+      # Exibir o gráfico
+      plt.show()
+
+       
+    
+    
+    
+  
+    
